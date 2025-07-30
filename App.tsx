1695968f195b5473dbc0bf7b46e0bd5c5fccc681
razan-sameh <razansameh53@gmail.com>
2025-07-30 20:50:37 +0300

    update deployed server

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { store } from './src/redux/store';
 import React from 'react';
 import AppNavigator from './src/navigator/AppNavigator';
 // export const serverURL = 'http://10.0.2.2:3000'; // Android emulators
-export const serverURL = 'http://192.168.1.6:3000'; // physical device
+// export const serverURL = 'http://192.168.1.6:3000'; // physical device
+export const serverURL = 'https://remittance-app-server-production.up.railway.app'; // deployed device
 
 export default function App() {
   return (
